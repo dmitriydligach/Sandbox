7f7a2ea5a6a77ca6aa77a94ea1ec3483ab72f2b9
dimdym <dimdym@gmail.com>
2019-10-31 10:29:19 -0500

    first draft of using pytorch dataloader class

---
+++ PyTorch/with_data_loader.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+import torch, glob, os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+data_pos = 'Imdb/train/pos/*.txt'
+data_neg = 'Imdb/train/neg/*.txt'
+
+base = os.environ['DATA_ROOT']
+data_pos = os.path.join(base, data_pos)
+data_neg = os.path.join(base, data_neg)
+
+# settings
+max_files = 1000
+max_features = 5000
+
+# hyper-parameters
+lr = 0.01
+batch_size = 100
+epochs = 10
+
+def load_data():
+  """Rotten tomatoes"""
+
+  labels = []
+  sentences = []
+
+  for file in glob.glob(data_pos)[:max_files]:
+    labels.append(1)
+    sentences.append(open(file).read().rstrip())
+  for file in glob.glob(data_neg)[:max_files]:
+    labels.append(0)
+    sentences.append(open(file).read())
+
+  return sentences, labels
+
+class ImdbDataset(Dataset):
+  """Used to fetch individual vectorized examples"""
+
+  def __init__(self):
+    """Constructor"""
+
+    sentences, labels = load_data()
+
+    x_train, x_dev, y_train, y_dev = \
+      train_test_split(
+      sentences,
+      labels,
+      test_size=0.1,
+      random_state=0)
+
+    vectorizer = TfidfVectorizer(
+      stop_words='english',
+      max_features=max_features,
+      ngram_range=(1, 1))
+
+    x_train = vectorizer.fit_transform(x_train)
+    x_dev = vectorizer.transform(x_dev)
+
+    self.x_train = torch.tensor(x_train.toarray()).float()
+    self.y_train = torch.tensor(y_train).float()
+    self.x_dev = torch.tensor(x_dev.toarray()).float()
+    self.y_dev = torch.tensor(y_dev).float()
+
+  def get_dev(self):
+    """Return the development set"""
+
+    return self.x_dev, self.y_dev
+
+  def __len__(self):
+    return len(self.x_train)
+
+  def __getitem__(self, index):
+    """Primary entry point for PyTorch datasets"""
+
+    return self.x_train[index], self.y_train[index]
+
+class Perceptron(nn.Module):
+  """A Perceptron is one Linear layer"""
+
+  def __init__(self, input_dim):
+      """Args: input_dim (int): size of the input features"""
+
+      super(Perceptron, self).__init__()
+      self.fc1 = nn.Linear(input_dim, 1)
+
+  def forward(self, x):
+    """x.shape should be (batch, input_dim)"""
+
+    return torch.sigmoid(self.fc1(x))
+
+def train():
+  """Training loop"""
+
+  perceptron = Perceptron(input_dim=max_features)
+  optimizer = optim.Adam(params=perceptron.parameters(), lr=lr)
+  bce_loss = nn.BCELoss()
+
+  dataset = ImdbDataset()
+
+  x_dev, y_dev = dataset.get_dev()
+
+  dataloader = DataLoader(
+    dataset=dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True)
+
+  # do several passess over training examples
+  for epoch in range(epochs):
+
+    # do one pass over training examples
+    for x_train, y_train in dataloader:
+      optimizer.zero_grad()
+
+      y_predicted = perceptron(x_train).squeeze()
+      loss = bce_loss(y_predicted, y_train)
+
+      loss.backward()
+      optimizer.step()
+
+    predictions = perceptron(x_dev).squeeze()
+    predictions = predictions > 0.5
+    acc = accuracy_score(y_dev, predictions.tolist())
+
+    print('ep: {}, loss: {}, acc: {}'.format(epoch+1, loss.item(), acc))
+
+if __name__ == "__main__":
+
+  train()
