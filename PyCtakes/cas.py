c3912a36d35858c17dde85656c46e6e2dd62928a
dimdym <dimdym@gmail.com>
2020-01-27 11:16:36 -0600

    testing cassis

---
+++ PyCtakes/cas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from cassis import *
+
+if __name__ == "__main__":
+
+  ts_file = open('TypeSystem.xml', 'rb')
+  type_system = load_typesystem(ts_file)
+
+  xmi_file = open('patientX_doc3_NOTE.txt.xmi', 'rb')
+  cas = load_cas_from_xmi(xmi_file, typesystem=type_system)
+
+  for sentence in cas.select('org.apache.ctakes.typesystem.type.textspan.Sentence'):
+    print('\nsentence:', sentence.get_covered_text())
+
+    # for token in cas.select_covered('org.apache.ctakes.typesystem.type.syntax.BaseToken', sentence):
+    for token in cas.select_covered('org.apache.ctakes.typesystem.type.syntax.WordToken', sentence):
+      print('token:', token.get_covered_text())
