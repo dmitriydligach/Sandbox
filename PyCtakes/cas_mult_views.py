05995e4b102ca0b0869f0e8dfdd07e02d30fb48c
dimdym <dimdym@gmail.com>
2020-01-30 15:18:17 -0600

    put xmis in separate dirs

---
+++ PyCtakes/cas_mult_views.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from cassis import *
+
+if __name__ == "__main__":
+
+  ts_file = open('TypeSystem.xml', 'rb')
+  type_system = load_typesystem(ts_file)
+
+  xmi_file = open('XmiMultViews/patientX_doc1_RAD.xmi', 'rb')
+  # xmi_file = open('/Users/Dima/Loyola/Data/Thyme/Xmi/ID075_clinic_223.xmi', 'rb')
+  cas = load_cas_from_xmi(xmi_file, typesystem=type_system)
+
+  for sentence in cas.select('org.apache.ctakes.typesystem.type.textspan.Sentence'):
+    print('\nsentence:', sentence.get_covered_text())
+
+    # for token in cas.select_covered('org.apache.ctakes.typesystem.type.syntax.BaseToken', sentence):
+    for token in cas.select_covered('org.apache.ctakes.typesystem.type.syntax.WordToken', sentence):
+      print('token:', token.get_covered_text())
