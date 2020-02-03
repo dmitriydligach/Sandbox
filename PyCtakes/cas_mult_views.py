422312d769e922860bde734f916feabe40bd38f7
dimdym <dimdym@gmail.com>
2020-02-03 13:25:34 -0600

    this is now working -- can read multiple views

--- PyCtakes/cas_mult_views.py
+++ PyCtakes/cas_mult_views.py
@@ -7,5 +7,9 @@ if __name__ == "__main__":
   ts_file = open('TypeSystem.xml', 'rb')
   type_system = load_typesystem(ts_file)
 
-  xmi_file = open('XmiMultViews/ID001_path_002.xmi', 'rb')
+  xmi_file = open('XmiMultViews/ID001_clinic_001.xmi', 'rb')
   cas = load_cas_from_xmi(xmi_file, typesystem=type_system)
+  gold_view = cas.get_view("GoldView")
+
+  for event in gold_view.select('org.apache.ctakes.typesystem.type.textsem.EventMention'):
+    print(event.get_covered_text())
