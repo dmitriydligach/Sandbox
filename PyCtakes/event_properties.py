9ff922d9f7bf5e5d171e8acae5889dae71213f5f
dimdym <dimdym@gmail.com>
2020-02-07 10:40:29 -0600

    can get annotations from different views

--- PyCtakes/event_properties.py
+++ PyCtakes/event_properties.py
@@ -4,7 +4,8 @@ from cassis import *
 
 type_system_path = './TypeSystem.xml'
 xmi_path = './XmiMultViews/ID001_clinic_001.xmi'
-type = 'org.apache.ctakes.typesystem.type.textsem.EventMention'
+event_type = 'org.apache.ctakes.typesystem.type.textsem.EventMention'
+sent_type = 'org.apache.ctakes.typesystem.type.textspan.Sentence'
 
 if __name__ == "__main__":
 
@@ -13,9 +14,14 @@ if __name__ == "__main__":
 
   xmi_file = open(xmi_path, 'rb')
   cas = load_cas_from_xmi(xmi_file, typesystem=type_system)
-  gold_view = cas.get_view("GoldView")
+  gold_view = cas.get_view('GoldView')
+  sys_view = cas.get_view('_InitialView')
 
-  for event in gold_view.select(type):
-    print(event.get_covered_text())
-    print('misc:', event.event.properties.docTimeRel)
+  for sentence in sys_view.select(sent_type):
+    print('sentence:', sentence.get_covered_text())
+
+    for event in gold_view.select_covered(event_type, sentence):
+      text = event.get_covered_text()
+      dtr = event.event.properties.docTimeRel
+      print('{} - {}'.format(text, dtr))
     print()
