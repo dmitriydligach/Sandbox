a38436166fb5fb58f9f7168950f3f3e76a3dae7f
Dima <ddligach@luc.edu>
2021-01-04 17:09:12 -0600

    reformatted and changed examples

--- T5/examples.py
+++ T5/examples.py
@@ -1,21 +1,35 @@
+import argparse
+
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-tokenizer = T5Tokenizer.from_pretrained('t5-11b')
-model = T5ForConditionalGeneration.from_pretrained('t5-11b')
+if __name__ == "__main__":
+  "My kind of street"
 
-input_ids = tokenizer('The <extra_id_0> walks in <extra_id_1> park', return_tensors='pt').input_ids
-labels = tokenizer('<extra_id_0> cute dog <extra_id_1> the <extra_id_2> </s>', return_tensors='pt').input_ids
+  hparam_dict = dict(model_name='t5-11b')
+  hparams = argparse.Namespace(**hparam_dict)
+  print('hyper-parameters:', hparams)
 
-outputs = model(input_ids=input_ids, labels=labels)
-loss = outputs.loss
-logits = outputs.logits
+  tokenizer = T5Tokenizer.from_pretrained(hparams.model_name)
+  model = T5ForConditionalGeneration.from_pretrained(hparams.model_name)
 
-s = "summarize: state authorities dispatched emergency crews tuesday to survey the damage after an onslaught of severe weather in mississippi."
-input_ids = tokenizer(s, return_tensors="pt").input_ids
-outputs = model.generate(input_ids)
-tokenizer.convert_ids_to_tokens(outputs.squeeze())
+  train_example = 'The <extra_id_0> walks in <extra_id_1> park'
+  train_labels = '<extra_id_0> cute dog <extra_id_1> the <extra_id_2> </s>'
 
-s = "translate English to German: one two three four five."
-input_ids = tokenizer(s, return_tensors="pt").input_ids
-outputs = model.generate(input_ids)
-tokenizer.convert_ids_to_tokens(outputs.squeeze())
+  input_ids = tokenizer(train_example, return_tensors='pt').input_ids
+  labels = tokenizer(train_labels, return_tensors='pt').input_ids
+
+  outputs = model(input_ids=input_ids, labels=labels)
+  loss = outputs.loss
+  logits = outputs.logits
+
+  text = "summarize: state authorities dispatched emergency " \
+         "crews tuesday to survey the damage after an onslaught " \
+         "of severe weather in mississippi."
+  input_ids = tokenizer(text, return_tensors="pt").input_ids
+  outputs = model.generate(input_ids)
+  print('summary:', tokenizer.decode(outputs.squeeze()))
+
+  s = "translate English to German: one two three four five."
+  input_ids = tokenizer(s, return_tensors="pt").input_ids
+  outputs = model.generate(input_ids)
+  print('translation:', tokenizer.decode(outputs.squeeze()))
