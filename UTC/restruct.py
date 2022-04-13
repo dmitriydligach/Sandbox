1f7df1d61c6333dc0f029963a0593e0bd2366639
Dima <ddligach@luc.edu>
2022-04-13 15:22:23 -0500

    utc code

---
+++ UTC/restruct.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os, datetime, shutil
+
+root_dir = '/Users/Dima/GDrive/UTC/'
+content_dir = 'Top Level/Engagement Team/Ukraine Teams Content/'
+target_dir = 'Top Level/Engagement Team/Media Files/'
+
+old_dir = os.path.join(root_dir, content_dir)
+new_dir = os.path.join(root_dir, target_dir)
+
+# set of allowed media file extensions
+media_files = {'jpeg', 'mp4', 'jpg', 'mov'}
+
+def main():
+  """Iterate the hell out of it"""
+
+  for path, dirs, files in os.walk(old_dir):
+    if 'Week of' not in path:
+      continue # only look at subfolders like 'Week of March 21-March 27'
+
+    for file_name in files:
+      orig_path = os.path.join(path, file_name)
+      file_name = os.path.basename(orig_path)
+      elements = file_name.split('.')
+      old_name = elements[0]
+      extension = elements[-1]
+
+      if extension.lower() not in media_files:
+        continue
+
+      mod_time = os.path.getmtime(orig_path)
+      human_time = datetime.datetime.fromtimestamp(mod_time)
+      date_only = str(human_time).split(' ')[0]
+
+      new_path = '%s%s-%s.%s' % (new_dir, date_only, old_name, extension)
+      print(orig_path)
+      print(new_path)
+      print('=' * 120)
+
+      try:
+        shutil.copy(orig_path, new_path)
+      except:
+        print('couldnot copy:', orig_path)
+
+if __name__ == "__main__":
+
+  main()
