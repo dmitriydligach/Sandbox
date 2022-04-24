97eb5913ed6fb4fbc0b3a4676a4443886b7fecbf
Dima <ddligach@luc.edu>
2022-04-24 08:23:20 -0500

    switched to bert training and increased batch size

--- MLM/run.sh
+++ MLM/run.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# comment param below to train from scratch \
-# --model_name_or_path roberta-base \
-
 python3 run_mlm.py \
-    --model_type roberta \
-    --config_name roberta-base \
-    --tokenizer_name roberta-base \
+    --model_type bert \
+    --config_name bert-base-uncased \
+    --tokenizer_name bert-base-uncased \
     --dataset_name wikitext \
-    --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 24 \
-    --per_device_eval_batch_size 24 \
+    --dataset_config_name wikitext-103-raw-v1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --do_train \
     --do_eval \
     --output_dir Output \
     --overwrite_output_dir
+
