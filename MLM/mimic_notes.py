0544ba25f213411e0255f2fdddf8351b15541507
Dima <ddligach@luc.edu>
2022-05-19 11:07:11 -0500

    extracting all notes instead of first 1K

--- MLM/mimic_notes.py
+++ MLM/mimic_notes.py
@@ -8,7 +8,7 @@ def extract_notes():
   """Extract only mimic notes to a csv file"""
 
   outfile = open(out_file, 'w')
-  frame = pandas.read_csv(mimic_notes_file, dtype='str', nrows=1000)
+  frame = pandas.read_csv(mimic_notes_file, dtype='str', nrows=None)
 
   for text in frame.TEXT:
     printable = ''.join(c for c in text if c in string.printable)
