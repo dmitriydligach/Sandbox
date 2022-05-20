771c7b166ac4a7fdab906b01ed0b4474f0fa6320
Dima <ddligach@luc.edu>
2022-05-20 11:21:44 -0500

    added code to train tokenizer on mimic

--- MLM/train_tokenizer.py
+++ MLM/train_tokenizer.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
 
-import sys
-sys.dont_write_bytecode = True
-sys.path.append('../Anafora')
-
-import glob, os
-
-from tokenizers import CharBPETokenizer
+import os
+from pathlib import Path
+from tokenizers import ByteLevelBPETokenizer
 
 def train():
   """My main man"""
 
   base = os.environ['DATA_ROOT']
-  corpus_path = base + 'Thyme/Text/train+dev+test/*'
-  files = glob.glob(corpus_path)
+  corpus_path = base + 'MimicIII/Encounters/Text/'
+  files = [str(file) for file in Path(corpus_path).glob('*.txt')]
 
-  tokenizer = CharBPETokenizer(lowercase=True)
+  tokenizer = ByteLevelBPETokenizer(lowercase=True)
   tokenizer.train(
     files=files,
-    vocab_size=10000,
-    min_frequency=3,
-    show_progress=True)
-  tokenizer.save('./thyme-tokenizer')
+    vocab_size=30000,
+    min_frequency=2,
+    show_progress=True,
+    special_tokens=[
+      "<s>",
+      "</s>",
+      "<pad>",
+      "<unk>",
+      "<mask>"])
 
+  os.mkdir('./Tokenizer')
+  tokenizer.save_model('./Tokenizer')
 
 def test():
   """Test trained tokenizer"""
 
-  tokenizer = CharBPETokenizer(
-    './thyme-tokenizer-vocab.json',
-    './thyme-tokenizer-merges.txt')
+  tokenizer = ByteLevelBPETokenizer(
+    './Tokenizer/vocab.json',
+    './Tokenizer/merges.txt')
 
   vocab = tokenizer.get_vocab()
   print('vocab size:', len(vocab))
