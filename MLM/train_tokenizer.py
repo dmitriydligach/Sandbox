9654d5a80d3c887aebc9cd2968fc1320cff778e7
Dima <ddligach@luc.edu>
2022-05-25 10:51:08 -0500

    removed old code and added vocab size param

--- MLM/train_tokenizer.py
+++ MLM/train_tokenizer.py
@@ -3,10 +3,6 @@
 import os
 from pathlib import Path
 
-# old tokenizers API?
-from tokenizers import ByteLevelBPETokenizer
-
-# new tokenizers API?
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
@@ -20,54 +16,20 @@ def train():
   files = [str(file) for file in Path(corpus_path).glob('*.txt')]
 
   tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-  trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+
+  trainer = BpeTrainer(
+    vocab_size=30000,
+    min_frequency=2,
+    show_progress=True,
+    special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+
   tokenizer.pre_tokenizer = Whitespace()
+
   tokenizer.train(files, trainer)
 
   os.mkdir('./Tokenizer')
   tokenizer.save("Tokenizer/mimic.json")
-
-def train_old():
-  """My main man"""
-
-  base = os.environ['DATA_ROOT']
-  corpus_path = base + 'MimicIII/Encounters/Text/'
-  files = [str(file) for file in Path(corpus_path).glob('*.txt')]
-
-  tokenizer = ByteLevelBPETokenizer(lowercase=True)
-  tokenizer.train(
-    files=files,
-    vocab_size=30000,
-    min_frequency=2,
-    show_progress=True,
-    special_tokens=[
-      "<s>",
-      "</s>",
-      "<pad>",
-      "<unk>",
-      "<mask>"])
-
-  os.mkdir('./Tokenizer')
-  tokenizer.save_model('./Tokenizer')
-
-def test_old():
-  """Test trained tokenizer"""
-
-  tokenizer = ByteLevelBPETokenizer(
-    './Tokenizer/vocab.json',
-    './Tokenizer/merges.txt')
-
-  vocab = tokenizer.get_vocab()
-  print('vocab size:', len(vocab))
-
-  encoded = tokenizer.encode('patient dr. who diagnosed with brain abc')
-  encoded.pad(15)
-
-  print('encoded:', encoded.ids)
-  print('decoded:', tokenizer.decode(encoded.ids))
-
-  print(encoded.tokens)
-  print(encoded.attention_mask)
+  tokenizer.save_pretrained()
 
 def test():
   """Using the new API"""
@@ -79,5 +41,5 @@ def test():
 
 if __name__ == "__main__":
 
-  # train()
+  train()
   test()
