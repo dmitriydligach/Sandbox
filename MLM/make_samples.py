1faa9d962d32811f1fedf3062b4de726ae189bcd
Dima <ddligach@luc.edu>
2022-06-23 13:32:13 -0500

    added function to write all cui files into a single file

--- MLM/make_samples.py
+++ MLM/make_samples.py
@@ -1,12 +1,23 @@
 #! /usr/bin/env python3
-import pandas, string, os
+import pandas, string, os, pathlib
 
 # Create samples for pretraining transformers from MIMIC notes
 # For now each note is one sample, but will break down further later
 
 base = os.environ['DATA_ROOT']
+
 mimic_notes_file = os.path.join(base, 'MimicIII/Source/NOTEEVENTS.csv')
+mimic_cui_dir = os.path.join(base, 'MimicIII/Encounters/Cuis/All/')
 out_file = 'notes.txt'
+
+def extract_cuis():
+  """Each line is the content of one cui file"""
+
+  outfile = open(out_file, 'w')
+
+  for cui_file in pathlib.Path(mimic_cui_dir).glob('*.txt'):
+    cuis_from_file = pathlib.Path(cui_file).read_text()
+    outfile.write(cuis_from_file + '\n')
 
 def extract_notes():
   """Extract only mimic notes to a csv file"""
@@ -21,4 +32,4 @@ def extract_notes():
 
 if __name__ == "__main__":
 
-  extract_notes()
+  extract_cuis()
