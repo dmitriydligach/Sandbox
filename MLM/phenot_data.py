90ca22797e57b7ce73a7343d6e1bc069c5f8351b
Dima <ddligach@luc.edu>
2022-07-13 10:37:38 -0500

    turned data reader into a torch dataset

--- MLM/phenot_data.py
+++ MLM/phenot_data.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
 import os, pathlib
+from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
-class DatasetProvider:
+class PhenotypingDataset(Dataset):
   """Read data from files and make inputs/outputs"""
 
-  def __init__(self,
-               corpus_path,
-               tokenizer_path):
+  def __init__(self, corpus_path, tokenizer_path):
     """Load tokenizer and save corpus path"""
+
+    self.x = []
+    self.y = []
 
     self.corpus_path = corpus_path
     self.label2int = {'no':0, 'yes':1}
     self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
-  def load_as_int_seqs(self):
-    """Convert examples into lists of indices"""
+    self.load_examples()
 
-    x = []
-    y = []
+  def __len__(self):
+    """Requried by pytorch"""
+
+    assert(len(self.x) == len(self.y))
+    return len(self.x)
+
+  def __getitem__(self, index):
+    """Required by pytorch"""
+
+    return self.tokenizer.encode(self.x[index]), self.y[index]
+
+  def load_examples(self):
+    """Convert examples into lists of indices"""
 
     cui_counts = [] # n cuis in each sample
 
@@ -28,7 +40,7 @@ class DatasetProvider:
 
       for f in os.listdir(label_dir):
         int_label = self.label2int[d.lower()]
-        y.append(int_label)
+        self.y.append(int_label)
 
         file_path = os.path.join(label_dir, f)
         text = pathlib.Path(file_path).read_text()
@@ -36,18 +48,15 @@ class DatasetProvider:
         cui_list = text.split()
         cui_counts.append(len(cui_list))
         cui_list = [cui[1:] for cui in cui_list[:510]]
-
-        first_510_cuis_string = ' '.join(cui_list)
-        x.append(self.tokenizer.encode(first_510_cuis_string))
+        self.x.append(' '.join(cui_list))
 
     print('average number of cuis:', sum(cui_counts) / len(cui_counts))
-
-    return x, y
 
 if __name__ == "__main__":
 
   base = os.environ['DATA_ROOT']
   data_dir = os.path.join(base, 'Opioids1k/Train/')
+  tokenizer_path = 'Tokenizer'
 
-  dp = DatasetProvider(data_dir, 'Tokenizer')
-  x, y = dp.load_as_int_seqs()
+  dp = PhenotypingDataset(data_dir, tokenizer_path)
+  print(dp)
