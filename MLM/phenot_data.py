bf2dc142170b9a52e99029f56b764bc7d31a1e62
Dima <ddligach@luc.edu>
2022-07-20 12:56:25 -0500

    rewrote to be able to pass a list of files instead of dir name

--- MLM/phenot_data.py
+++ MLM/phenot_data.py
@@ -7,14 +7,22 @@ from transformers import AutoTokenizer
 class PhenotypingDataset(Dataset):
   """Read data from files and make inputs/outputs"""
 
-  def __init__(self, corpus_path, tokenizer_path):
+  def __init__(self, corpus_path_or_files, tokenizer_path):
     """Load tokenizer and save corpus path"""
 
     self.x = []
     self.y = []
 
+    self.file_paths = []
+
+    if type(corpus_path_or_files) == str:
+      self.file_paths = pathlib.Path(corpus_path_or_files).glob('*/*.txt')
+    elif type(corpus_path_or_files) == list:
+      self.file_paths = corpus_path_or_files
+    else:
+      print('wrong type!')
+
     self.label2int = {'no':0, 'yes':1}
-    self.corpus_path = corpus_path
     self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
     self.load_examples()
@@ -44,22 +52,19 @@ class PhenotypingDataset(Dataset):
     """Convert examples into lists of indices"""
 
     cui_counts = [] # n cuis in each sample
-    for d in os.listdir(self.corpus_path):
-      label_dir = os.path.join(self.corpus_path, d)
 
-      for f in os.listdir(label_dir):
-        int_label = self.label2int[d.lower()]
-        self.y.append(int_label)
+    for file_path in self.file_paths:
+      str_label = file_path.parts[-2].lower()
+      int_label = self.label2int[str_label]
+      self.y.append(int_label)
 
-        file_path = os.path.join(label_dir, f)
-        text = pathlib.Path(file_path).read_text()
+      text = file_path.read_text()
+      cui_list = text.split()
+      cui_counts.append(len(cui_list))
 
-        cui_list = text.split()
-        cui_counts.append(len(cui_list))
-
-        # strip 'C' and get first 510 CUIs
-        cui_list = [cui[1:] for cui in cui_list[:510]]
-        self.x.append(' '.join(cui_list))
+      # strip 'C' and get the first 510 CUIs
+      cui_list = [cui[1:] for cui in cui_list[:510]]
+      self.x.append(' '.join(cui_list))
 
     print('average number of cuis:', sum(cui_counts) / len(cui_counts))
 
