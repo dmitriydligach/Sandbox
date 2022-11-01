89a68a190a4ffa1b12e30a81efbbc06bd173becd
Dima <ddligach@luc.edu>
2022-11-01 11:01:45 -0500

    added a flag to use unique cuis rather than all cuis from encounters

--- MLM/make_samples.py
+++ MLM/make_samples.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-import pandas, string, os, pathlib
+import pandas, string, os, pathlib, numpy
 
 # Create samples for pretraining transformers from MIMIC notes
 # For now each note is one sample, but will break down further later
@@ -10,9 +10,10 @@ mimic_notes_file = os.path.join(base, 'MimicIII/Source/NOTEEVENTS.csv')
 mimic_cui_dir = os.path.join(base, 'MimicIII/Encounters/Cuis/All/')
 out_file = 'notes.txt'
 
-def extract_cuis():
+def extract_cuis(use_unique_cuis=True):
   """Each line is the content of one cui file"""
 
+  cui_counts = []
   outfile = open(out_file, 'w')
 
   for cui_file in pathlib.Path(mimic_cui_dir).glob('*.txt'):
@@ -20,9 +21,20 @@ def extract_cuis():
 
     # remove first C from each CUI to avoid tokenization problems
     cui_list = [cui[1:] for cui in cuis_from_file.split()]
+
+    if use_unique_cuis:
+      cui_list = list(set(cui_list))
+
+    cui_counts.append(len(cui_list))
     cui_string = ' '.join(cui_list)
 
     outfile.write(cui_string + '\n')
+
+  print(f'unique cui flag is {use_unique_cuis}')
+  print(f'average number of cuis: {numpy.mean(cui_counts):.2f}')
+  print(f'median number of cuis: {numpy.median(cui_counts):.2f}')
+  print(f'standard deviation: {numpy.std(cui_counts):.2f}')
+  print(f'max number of cuis: {numpy.max(cui_counts)}')
 
 def extract_notes():
   """Extract only mimic notes to a csv file"""
