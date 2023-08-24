771f8ed1f76aa3eb8bbde2ee1faab4f052692f0d
Dima <ddligach@luc.edu>
2023-08-24 09:52:01 -0500

    added subjective and objective section text

--- Lama/drbench.py
+++ Lama/drbench.py
@@ -5,15 +5,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from time import time
 from rouge_score import rouge_scorer
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 drbench_path = 'DrBench/Csv/summ_0821_dev.csv'
 model_path = '/home/dima/Lama/Models/Llama-2-13b-chat-hf'
 
-system_prompt = \
-  """You are a physician. Please provide a concise summary of problems/diagnoses
-  based on the assessment below. Format the output as a bullet point list."""
+system_prompt = 'You are a physician. Please provide a concise summary ' \
+                'of the problems/diagnoses based on the progress note text ' \
+                'below. Format the output as a bullet point list.'
 
 def calc_rougel(generated_text, reference_text):
   """Compute Rouge-L score"""
@@ -44,10 +43,36 @@ def csv_to_assm_sum_tuples():
 
   return ios
 
+def csv_to_io_tuples():
+  """Get summarization input/output pair tuples"""
+
+  data_csv = os.path.join(base_path, drbench_path)
+  df = pandas.read_csv(data_csv, dtype='str')
+
+  # input/output pairs
+  ios = []
+
+  for assm, summ, subj, obj in \
+    zip(df['Assessment'], df['Summary'], df['S'], df['O']):
+
+    # sometimes assm is empty and pandas returns a float
+    if type(assm) == str and type(summ) == str \
+       and type(subj) == str and type(obj) == str:
+
+      assm = ''.join(c for c in assm if c in string.printable)
+      summ = ''.join(c for c in summ if c in string.printable)
+      subj = ''.join(c for c in subj if c in string.printable)
+      obj = ''.join(c for c in obj if c in string.printable)
+
+      input_text = f'### Subjective Section ###\n\n{subj}\n\n' \
+                   f'### Objective Section ###\n\n{obj}\n\n' \
+                   f'### Assessment Section ###\n\n{assm}'
+      ios.append((input_text, summ))
+
+  return ios
+
 def main(inputs_outputs):
   """Ask for input and feed into llama2"""
-
-  start = time()
 
   tokenizer = AutoTokenizer.from_pretrained(model_path)
   model = AutoModelForCausalLM.from_pretrained(
@@ -61,14 +86,14 @@ def main(inputs_outputs):
     torch_dtype=torch.float16,
     device_map='auto')
 
-  end = time()
-  print(f'\n[model load time: {end - start} seconds]\n')
-
   f1s = []
-  for input_text, reference_output in inputs_outputs:
-    prompt = f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n\n{input_text} [/INST]\n\n'
+  inference_times = []
 
+  for input_text, reference_output in inputs_outputs:
+
+    prompt = f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n\n{input_text} [/INST]\n\n'
     start = time()
+
     generated_outputs = pipeline(
       prompt,
       do_sample=True,
@@ -76,28 +101,31 @@ def main(inputs_outputs):
       num_return_sequences=1,
       eos_token_id=tokenizer.eos_token_id,
       temperature=0.001,
-      max_length=500)
-    end = time()
+      max_length=2000)
 
-    # remove the the prompt from output
+    end = time()
+    inference_times.append(end - start)
+
+    print('\n********** begin generated text **********\n')
+    print(generated_outputs[0]['generated_text'])
+    print('\n********** end generated text **********\n')
+
+    print(f'reference summary: {reference_output}\n\n')
+
+    # remove the the prompt from output and evaluate
     end_index = generated_outputs[0]['generated_text'].index('[/INST]')
     generated_text = generated_outputs[0]['generated_text'][end_index+7:]
-
-    print('\n[********** begin generated text **********]\n')
-    print(generated_text)
-    print('\n[********** end generated text **********]\n')
-    print(f'reference summary: {reference_output}\n\n')
     f1 = calc_rougel(generated_text.lower(), reference_output.lower())
     f1s.append(f1)
-    print(f'f1={f1}')
 
-    print(f'[inference time: {end - start} seconds]\n')
-
+  av_inf_time = numpy.mean(inference_times)
+  print(f'\naverage inference time: {av_inf_time} seconds]')
   print('average f1:', numpy.mean(f1s))
 
 if __name__ == "__main__":
 
   base_path = os.environ['DATA_ROOT']
 
-  ios = csv_to_assm_sum_tuples()
+  ios = csv_to_io_tuples()
+
   main(ios)
